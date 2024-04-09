d282bc8624556e7356f21027a2aedf59875e63ea
KasperAPedersen <35560445+KasperAPedersen@users.noreply.github.com>
2024-04-09 20:59:34 +0200

    upda

--- database.js
+++ database.js
@@ -5,10 +5,10 @@ if(process.env.NODE_ENV !== 'production') {
 let mysql = require('mysql2');
 
 let mySqlPool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'mathias',
     multipleStatements: false
 });
 
