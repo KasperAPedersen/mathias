c409455d68e2af7820acacf947b22e8b201c992d
KasperAPedersen <35560445+KasperAPedersen@users.noreply.github.com>
2024-04-09 18:47:19 +0200

    Update

--- mathias.sql
+++ mathias.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Vært: 127.0.0.1
--- Genereringstid: 09. 04 2024 kl. 09:49:31
--- Serverversion: 10.4.28-MariaDB
--- PHP-version: 8.2.4
+-- Host: 127.0.0.1
+-- Generation Time: Apr 09, 2024 at 06:46 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur-dump for tabellen `assignments`
+-- Table structure for table `assignments`
 --
 
 CREATE TABLE `assignments` (
@@ -35,7 +35,7 @@ CREATE TABLE `assignments` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Data dump for tabellen `assignments`
+-- Dumping data for table `assignments`
 --
 
 INSERT INTO `assignments` (`id`, `userID`, `status`, `content`) VALUES
@@ -47,62 +47,57 @@ INSERT INTO `assignments` (`id`, `userID`, `status`, `content`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur-dump for tabellen `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
+  `picture` varchar(255) NOT NULL DEFAULT 'https://www.w3schools.com/w3css/img_avatar2.png',
+  `first` varchar(255) NOT NULL,
+  `last` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `pass` varchar(255) NOT NULL
+  `pass` varchar(255) NOT NULL,
+  `admin` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Data dump for tabellen `users`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `pass`) VALUES
-(1, 'a', 'a'),
-(2, 'a', 'b'),
-(3, 'c', 'd'),
-(4, '1', '2'),
-(5, 'test', '$2b$10$wAmXZcLfyT3xl7r2YU6pD.5Vd...dN7toDX9m1eY4jNAjBy3vhqZy'),
-(6, 't1', '$2b$10$7FF9aow8kuTdtEOGVvQVeOIyD6K3XNTgwJBmLMuKC8YbflAvlKva.'),
-(7, 'admin', '$2b$10$xWc73JM6K7bKcV403k2XkOJedvZvyq7ZprBWRXZmqLuVTASUk2Jcu'),
-(8, 'a', 'a'),
-(9, 'lebbe', '$2b$10$8AL0NcEZpSJ9AghI1fOmbuKMhDVcgLwPHbCgPQFYuKLGENPeXHRqi'),
-(10, 'rolle', '$2b$10$wXx2JcqFujFd2pBd8ijug.hW4hYfq3gz3BY77X6gSWShD/rzlW8iW');
+INSERT INTO `users` (`id`, `picture`, `first`, `last`, `name`, `pass`, `admin`) VALUES
+(24, 'https://www.w3schools.com/w3css/img_avatar2.png', 'Peter', 'Hansen', 'admin', '$2b$10$9rAlWh6ekPpi.za5.ns8Z.Mrnk4lxQFZTn.wh8rFWz0loCOJDiURW', 1);
 
 --
--- Begrænsninger for dumpede tabeller
+-- Indexes for dumped tables
 --
 
 --
--- Indeks for tabel `assignments`
+-- Indexes for table `assignments`
 --
 ALTER TABLE `assignments`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks for tabel `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- Brug ikke AUTO_INCREMENT for slettede tabeller
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- Tilføj AUTO_INCREMENT i tabel `assignments`
+-- AUTO_INCREMENT for table `assignments`
 --
 ALTER TABLE `assignments`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- Tilføj AUTO_INCREMENT i tabel `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
